ea418800ce6488023e38687221d47a6044a6af98
julioan1979-droid <julioan1979@gmail.com>
2025-11-01 23:03:51 +0000

    Simplifica sincronizacao do periodo com o widget

--- pages/home.py
+++ pages/home.py
@@ -1091,16 +1091,20 @@ def dashboard_tesoureiro(dados: dict):
         if periodo_atual != st.session_state.get(periodo_key):
             st.session_state[periodo_key] = periodo_atual
 
-        def _periodo_para_widget(periodo: tuple[date, date]) -> tuple[datetime, datetime]:
+        def _periodo_para_widget(periodo: tuple[date, date]) -> tuple[date, date]:
             return (
-                pd.Timestamp(periodo[0]).to_pydatetime(),
-                pd.Timestamp(periodo[1]).to_pydatetime(),
+                pd.Timestamp(periodo[0]).date(),
+                pd.Timestamp(periodo[1]).date(),
             )
 
-        st.session_state.setdefault(periodo_widget_key, _periodo_para_widget(periodo_atual))
+        widget_val = _periodo_para_widget(periodo_atual)
+        if st.session_state.get(periodo_widget_key) != widget_val:
+            st.session_state[periodo_widget_key] = widget_val
+
         widget_atual = _normalizar_periodo(st.session_state.get(periodo_widget_key, periodo_atual))
         if widget_atual != periodo_atual:
-            st.session_state[periodo_widget_key] = _periodo_para_widget(periodo_atual)
+            st.session_state[periodo_key] = widget_atual
+            periodo_atual = widget_atual
 
         data_inicio, data_fim = periodo_atual
         data_inicio_ts = pd.Timestamp(data_inicio)
@@ -1195,7 +1199,6 @@ def dashboard_tesoureiro(dados: dict):
         def _definir_periodo(inicio: date, fim: date) -> None:
             periodo = (inicio, fim)
             st.session_state[periodo_key] = periodo
-            st.session_state[periodo_widget_key] = _periodo_para_widget(periodo)
             st.experimental_rerun()
 
         filtro_cols = st.columns([2, 3])
@@ -1209,7 +1212,6 @@ def dashboard_tesoureiro(dados: dict):
             periodo_normalizado = _normalizar_periodo(periodo_escolhido)
             if periodo_normalizado != st.session_state.get(periodo_key):
                 st.session_state[periodo_key] = periodo_normalizado
-                st.session_state[periodo_widget_key] = _periodo_para_widget(periodo_normalizado)
                 st.experimental_rerun()
 
         with filtro_cols[1]:
